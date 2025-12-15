ab8c3de18e40b2efd07a132e001f0eab7208a309
Anna Ovcharenko <anyutka1426@gmail.com>
2025-12-15 16:55:56 +0200

    fix4

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,8 @@ import "./globals.css";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import React from "react";
 
-interface LayoutProps extends React.PropsWithChildren {
-  modal: React.ReactNode;
-}
-
-export default function RootLayout({ children, modal }: LayoutProps) {
+export default function RootLayout({ children, modal }) {
   return (
     <html lang="uk">
       <body>
